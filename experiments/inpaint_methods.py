6062fd3508bf6166ec2a5623cb4d37d905201fb2
RomainCrgnl <romain.cargnello1@gmail.com>
2026-03-26 18:24:23 +0100

    Clear experiments

--- experiments/inpaint_methods.py
+++ experiments/inpaint_methods.py
@@ -329,35 +329,6 @@ import IPython
 import matplotlib.pyplot as plt
 
 
-def viewimage(
-    im, normalize=True, vmin=-1, vmax=1, z=1, order=0, titre="", displayfilename=False
-):
-    im = im.detach().cpu().permute(2, 3, 1, 0).squeeze()
-    imin = np.array(im).astype(np.float32)
-    channel_axis = 2 if len(im.shape) > 2 else None
-    if z != 1:
-        from skimage.transform import rescale
-
-        imin = rescale(imin, z, order=order, channel_axis=channel_axis)
-    if normalize:
-        if vmin is None:
-            vmin = imin.min()
-        if vmax is None:
-            vmax = imin.max()
-        if np.abs(vmax - vmin) > 1e-10:
-            imin = (imin.clip(vmin, vmax) - vmin) / (vmax - vmin)
-        else:
-            imin = vmin
-    else:
-        imin = imin.clip(0, 255) / 255
-    imin = (imin * 255).astype(np.uint8)
-    filename = tempfile.mktemp(titre + ".png")
-    if displayfilename:
-        print(filename)
-    plt.imsave(filename, imin, cmap="gray")
-    IPython.display.display(IPython.display.Image(filename))
-
-
 def run_diffpir_inpaint(
     img_path: str,
     cfg: MethodConfig,
@@ -538,8 +509,6 @@ def run_diffpir_inpaint(
                     sqrt_1m_alphas_cumprod[t_i] ** 2
                     - sqrt_ae**2 * sqrt_1m_alphas_cumprod[t_im1] ** 2
                 ) * torch.randn_like(x)
-
-            # viewimage(torch.cat((x0, y), dim=3))
 
         x_0 = x / 2 + 0.5
 
